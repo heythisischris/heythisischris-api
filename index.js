1cfc5d4e431f67c30a0a0a8feb493462b7984f8e
Chris Aitken <chris@heythisischris.com>
2022-12-03 15:24:23 -0500

    Added custom script to convert Notion to HTML, syncing GitHub calendar

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import { SES } from '@aws-sdk/client-ses';
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { S3 } from "@aws-sdk/client-s3";
+import { CloudFront } from "@aws-sdk/client-cloudfront";
 import { unmarshall } from '@aws-sdk/util-dynamodb';
 import { randomUUID } from 'crypto';
-import NotionPageToHtml from 'notion-page-to-html';
+import { convertNotionBlocksToHtml } from './notion.js';
 
+const dynamodb = new DynamoDB();
+const s3 = new S3();
+const cloudfront = new CloudFront();
 const ses = new SES({ region: 'us-east-1' });
-const query = async (Statement, Parameters) => { return (await new DynamoDB().executeStatement({ Statement, Parameters })).Items.map(obj => unmarshall(obj)); };
+const query = async (Statement, Parameters) => { return (await dynamodb.executeStatement({ Statement, Parameters })).Items.map(obj => unmarshall(obj)); };
 
 export const handler = async (event) => {
     console.log('heythisischris init');
+    const lambdaStart = new Date();
     event.body ? event.body = JSON.parse(event.body) : event.body = {};
 
     if (event.path === '/githubSync') {
@@ -107,7 +113,11 @@ export const handler = async (event) => {
             },
         });
 
-        return { statusCode: 200, body: JSON.stringify('success'), headers: { 'Access-Control-Allow-Origin': '*' } };
+        return {
+            statusCode: 200,
+            body: JSON.stringify('success'),
+            headers: { 'Access-Control-Allow-Origin': '*' },
+        };
     }
     else if (event.path === '/comments') {
         let response = [{}];
@@ -121,19 +131,33 @@ export const handler = async (event) => {
         }
         else if (event.httpMethod === 'POST') {
             const id = randomUUID().slice(0, 8);
-            await query(`INSERT INTO heythisischris VALUE {'type':'comment', 'timestamp':'${new Date().toISOString()}', 'ip_address':'${event.requestContext.identity.sourceIp}', 'name':'${event.body.name.substr(0, 10)}', 'content':'${event.body.content.substr(0, 200)}', 'post_id':'${event.body.post_id}', 'id':'${id}'}`);
+            await query(`INSERT INTO heythisischris VALUE {
+                'type':'comment',
+                'timestamp':'${new Date().toISOString()}',
+                'ip_address':'${event.requestContext.identity.sourceIp}',
+                'name':'${event.body.name.substr(0, 10)}',
+                'content':'${event.body.content.substr(0, 200)}',
+                'post_id':'${event.body.post_id}',
+                'id':'${id}'
+            }`);
             const post = (await query(`SELECT * FROM heythisischris WHERE "type"='post' AND "id"='${event.body.post_id}'`))[0];
             await query(`UPDATE heythisischris SET "commentCount"=${post.commentCount+1} WHERE "type"='post' AND "timestamp"='${post.timestamp}' AND "id"='${event.body.post_id}'`);
-            response = [{ id: id }];
+            response = [{ id }];
         }
         else if (event.httpMethod === 'DELETE') {
             await query(`DELETE FROM heythisischris WHERE "type"='comment' AND "timestamp"='${event.body.timestamp}' AND "id"='${event.body.id}' AND "ip_address"='${event.requestContext.identity.sourceIp}'`);
             const post = (await query(`SELECT * FROM heythisischris WHERE "type"='post' AND "id"='${event.body.post_id}'`))[0];
             await query(`UPDATE heythisischris SET "commentCount"=${post.commentCount-1} WHERE "type"='post' AND "timestamp"='${post.timestamp}' AND "id"='${event.body.post_id}'`);
         }
-        return { statusCode: 200, body: JSON.stringify(response), headers: { 'Access-Control-Allow-Origin': '*' } };
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response),
+            headers: { 'Access-Control-Allow-Origin': '*' },
+        };
     }
     else if (event.path === '/notion') {
+        let response = '';
+
         const dynamodbPosts = [
             ...(await query(`SELECT "id", "timestamp", "type", "notion_id", "last_edited_time" from heythisischris WHERE "type"='post' ORDER BY "timestamp" ASC`)),
             ...(await query(`SELECT "id", "timestamp", "type", "notion_id", "last_edited_time" from heythisischris WHERE "type"='app' ORDER BY "timestamp" ASC`)),
@@ -151,27 +175,70 @@ export const handler = async (event) => {
             const dynamodbPost = dynamodbPosts.find(({ notion_id }) => notion_id === notionPost.id);
             if (!dynamodbPost) {
                 //we must replicate the notion page as a row in dynamodb
-                const { html } = await NotionPageToHtml.convert(`https://www.notion.so/heythisischris/${notionPost.id}`, { bodyContentOnly: true });
-                await query(`INSERT INTO heythisischris VALUE {'type':'${notionPost.type}', 'timestamp':'${notionPost.type==='post'?new Date().toISOString() : notionPosts.filter(obj=>obj.type==='app').length}', 'id':'${notionPost.title.toLowerCase().replace(/ /g,'-')}', 'notion_id':'${notionPost.id.replace(/-/g,'')}', 'title': '${notionPost.title}', 'commentCount': 0, 'content':?}`, [{ S: html }]);
-                console.log(`Added "${notionPost.title}"`);
+                const html = await convertNotionBlocksToHtml(notionPost.id);
+                await query(`INSERT INTO heythisischris VALUE {
+                    'type':'${notionPost.type}', 
+                    'timestamp':'${notionPost.type==='post'?new Date().toISOString() : notionPosts.filter(obj=>obj.type==='app').length}', 
+                    'id':'${notionPost.title.toLowerCase().replace(/ /g,'-')}', 
+                    'notion_id':'${notionPost.id.replace(/-/g,'')}', 
+                    'title':'${notionPost.title}', 
+                    'commentCount':0, 
+                    'content':?
+                }`, [{ S: html }]);
+                response += `added '${notionPost.title}', `;
             }
-            else if (notionPost.last_edited_time !== dynamodbPost.last_edited_time) {
-                //the post has been edited, so we must updated the title and content
-                const { html } = await NotionPageToHtml.convert(`https://www.notion.so/heythisischris/${dynamodbPost.notion_id}`, { bodyContentOnly: true });
-                await query(`UPDATE heythisischris SET "title"='${notionPost.title}', "content"=?, "last_edited_time"='${notionPost.last_edited_time}' WHERE "type"='${dynamodbPost.type}' AND "timestamp"='${dynamodbPost.timestamp}' AND "id"='${dynamodbPost.id}'`, [{ S: html }]);
-                console.log(`Updated "${notionPost.title}"`);
+            else if (notionPost.last_edited_time !== dynamodbPost.last_edited_time || event.queryStringParameters?.force) {
+                //the post has been edited (or we're forcing a global update), so we must update the title and content
+                const html = await convertNotionBlocksToHtml(dynamodbPost.notion_id);
+                await query(`UPDATE heythisischris SET 
+                    "title"='${notionPost.title}', 
+                    "content"=?, 
+                    "last_edited_time"='${notionPost.last_edited_time}' 
+                WHERE "type"='${dynamodbPost.type}' AND "timestamp"='${dynamodbPost.timestamp}' AND "id"='${dynamodbPost.id}'`, [{ S: html }]);
+                response += `updated '${notionPost.title}', `;
             }
         }
 
-        //remove deleted notion posts
-        for (const dynamodbPost of dynamodbPosts.filter(obj => !notionPosts.map(({ id }) => id).includes(obj.notion_id))) {
+        //remove deleted notion posts from dynamodb
+        for (const dynamodbPost of dynamodbPosts.filter(({ notion_id }) => !notionPosts.map(({ id }) => id).includes(notion_id))) {
             await query(`DELETE FROM heythisischris WHERE "type"='${dynamodbPost.type}' AND "timestamp"='${dynamodbPost.timestamp}'`);
-            console.log(`Deleted "${dynamodbPost.id}"`);
+            response += `deleted '${dynamodbPost.id}', `;
         }
 
+        const lambdaDuration = new Date(new Date() - lambdaStart);
+        const message = `Successfully ran in ${(lambdaDuration.getTime() / 1000).toFixed(2)} seconds: ${response.length ? response: 'no updates detected'}.`;
+        console.log(message);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message }),
+            headers: { 'Access-Control-Allow-Origin': '*' },
+        };
+    }
+    else if (event.path === '/githubCalendar') {
+        const image = await (await (await fetch('https://ghchart.rshah.org/heythisischris')).blob()).arrayBuffer();
+        await s3.putObject({
+            Bucket: 'heythisischris-files',
+            Key: 'githubcalendar.svg',
+            ContentType: 'image/svg+xml',
+            Body: image,
+        });
+        await cloudfront.createInvalidation({
+            DistributionId: 'EXIG3NHS7PGXY',
+            InvalidationBatch: {
+                CallerReference: Date.now().toString(),
+                Paths: {
+                    Quantity: 1,
+                    Items: ['/githubcalendar.svg']
+                }
+            }
+        });
         return;
     }
     else {
-        return { statusCode: 200, body: 'whatcha lookin for?', headers: { 'Access-Control-Allow-Origin': '*' } };
+        return {
+            statusCode: 200,
+            body: 'Howdy!',
+            headers: { 'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*' },
+        };
     }
 };
