8b18e4e3297a56a83224c81176e056beb7f8136d
Chris Aitken <chris@heythisischris.com>
2023-07-20 16:24:51 -0400

    Added back internal calendar route to store GitHub contributions as SVG

--- routers/internalRouter.ts
+++ routers/internalRouter.ts
@@ -1,8 +1,11 @@
 import { event } from "#src/utils/event";
-import { github } from "#src/routes/internal";
+import { github, calendar } from "#src/routes/internal";
 
 export const internalRouter = async () => {
     if (event.path.endsWith('/github')) {
         return github();
+    }
+    else if (event.path.endsWith('/calendar')) {
+        return calendar();
     }
 }
