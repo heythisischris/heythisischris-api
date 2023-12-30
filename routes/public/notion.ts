fc469169c368d630b2e3bb5d4ef047eaefcb702c
Christopher Aitken <chris@heythisischris.com>
2023-12-29 18:42:28 -0600

    Updated Notion sync, moving embedded images to S3 & supporting columns

--- routes/public/notion.ts
+++ routes/public/notion.ts
@@ -1,6 +1,10 @@
+import { event } from '#src/utils';
 import { notionSync } from "../scheduled"
 
 export const notion = async () => {
-    await notionSync();
-    return `Successfully synced`;
+    const startDate = new Date();
+    await notionSync({ tables: event.queryStringParameters.tables.split(',') });
+    const endDate = new Date();
+    const duration = endDate.getTime() - startDate.getTime();
+    return `Successfully synced in ${duration}ms`;
 }
--- routes/scheduled/notionSync.ts
+++ routes/scheduled/notionSync.ts
@@ -1,4 +1,7 @@
 import { client, notion, sleep } from "#src/utils";
+import { S3 } from "@aws-sdk/client-s3";
+import { getFileExtension } from '#src/utils/getFileExtension';
+const s3 = new S3();
 
 const typesDict = {
     apps: {
@@ -12,6 +15,7 @@ const typesDict = {
         bulleted_list_item: [`<li style='margin-left:30px'>`, `</li>`],
     },
 };
+
 const convertAnnotations = (annotations, href) => {
     if (href) {
         return [`<a target="_blank" rel="noreferrer" href="${href}">`, `</a>`];
@@ -30,32 +34,79 @@ const convertAnnotations = (annotations, href) => {
     }
 };
 
-export const notionSync = async () => {
+export const notionSync = async ({ tables } = { tables: ['apps', 'posts'] }) => {
     await client.connect();
-    for (const { databaseId, table } of [
-        { databaseId: 'cd84b4ed1d59460a8ee9070a4156cd3a', table: 'apps' },
-        { databaseId: '10378f9e5cfb4174ba811c9a52574741', table: 'posts' },
-    ]) {
+    const selectedTables = [];
+    if (tables.includes('apps')) {
+        selectedTables.push({ databaseId: 'cd84b4ed1d59460a8ee9070a4156cd3a', table: 'apps' });
+    }
+    if (tables.includes('posts')) {
+        selectedTables.push({ databaseId: '10378f9e5cfb4174ba811c9a52574741', table: 'posts' });
+    }
+
+    const formatBlock = async ({ table, block }) => {
+        let response = '';
+        if (block.type === 'image') {
+            const title = block.image.caption?.[0]?.plain_text ?? '';
+            let imageSource = block.image?.external?.url;
+            if (block.image?.file?.url) {
+                const strippedUrl = block.image.file.url.split('?')?.[0].replace('https://prod-files-secure.s3.us-west-2.amazonaws.com/', '');
+                const notionImageKey = strippedUrl.split('/')[1].replaceAll('-', '');
+                const notionImageExtension = strippedUrl.split('.')[1];
+                const s3Key = `${notionImageKey}.${notionImageExtension}`;
+                imageSource = `https://files.heythisischris.com/${s3Key}`;
+                try {
+                    await s3.headObject({ Bucket: 'heythisischris-files', Key: s3Key });
+                }
+                catch (err) {
+                    // We need to upload the image to our own S3 bucket. Let's do it.
+                    const notionImage = await (await (await fetch(block.image?.file?.url)).blob()).arrayBuffer();
+                    await s3.putObject({
+                        Bucket: 'heythisischris-files',
+                        Key: s3Key,
+                        ContentType: getFileExtension(notionImageExtension),
+                        Body: notionImage,
+                    });
+                }
+            }
+            response += `<img class="thumbnail" src="${imageSource}" title="${title}" />`;
+        }
+        else {
+            const [startTag, endTag] = typesDict[table][block.type];
+            response += startTag;
+            for (const element of block[block.type].rich_text) {
+                if (element.type === 'text') {
+                    const [startTag, endTag] = convertAnnotations(element.annotations, element.href);
+                    response += `${startTag}${element.text.content}${endTag}`;
+                }
+            }
+            response += endTag;
+        }
+        return response;
+    }
+
+    for (const { databaseId, table } of selectedTables) {
         const notionIds = (await notion.databases.query({ database_id: databaseId }))?.results?.map(({ id }) => id);
         await Promise.allSettled(notionIds.map(async (notionId, index) => {
             await sleep(index * 333);
             const blocks = (await notion.blocks.children.list({ block_id: notionId }))?.results;
             let response = '';
             for (const block of blocks) {
-                if (block.type === 'image') {
-                    const title = block.image.caption.length ? block.image.caption[0].plain_text : '';
-                    response += `<img class="thumbnail" src="${block.image.external.url}" title="${title}" />`;
-                }
-                else {
-                    const [startTag, endTag] = typesDict[table][block.type];
-                    response += startTag;
-                    for (const element of block[block.type].rich_text) {
-                        if (element.type === 'text') {
-                            const [startTag, endTag] = convertAnnotations(element.annotations, element.href);
-                            response += `${startTag}${element.text.content}${endTag}`;
+                if (block.has_children) {
+                    response += `<div class="flex flex-wrap gap-2">`;
+                    const innerBlocks = (await notion.blocks.children.list({ block_id: block.id })).results;
+                    for (const innerBlock of innerBlocks) {
+                        if (innerBlock.has_children) {
+                            const inmostBlocks = (await notion.blocks.children.list({ block_id: innerBlock.id })).results;
+                            for (const inmostBlock of inmostBlocks) {
+                                response += (await formatBlock({ table, block: inmostBlock }));
+                            }
                         }
                     }
-                    response += endTag;
+                    response += `</div>`;
+                }
+                else {
+                    response += (await formatBlock({ table, block }));
                 }
             }
             await client.query(`UPDATE "${table}" SET "content"=$1 WHERE "notion_id"=$2`, [response, notionId]);
